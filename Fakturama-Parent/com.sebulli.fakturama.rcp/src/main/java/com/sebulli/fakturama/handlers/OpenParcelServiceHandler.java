b2448bba9985ee6662ebefaa7a2ba50b7578b7e9
Ralf Heydenreich <rheydenr@justmail.de>
2018-06-07 23:49:20 +0200

    fixed parcel manager

--- Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/handlers/OpenParcelServiceHandler.java
+++ Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/handlers/OpenParcelServiceHandler.java
@@ -21,7 +21,6 @@ import javax.inject.Inject;
 import org.eclipse.e4.core.contexts.Active;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.services.nls.Translation;
 import org.eclipse.e4.ui.model.application.MApplication;
