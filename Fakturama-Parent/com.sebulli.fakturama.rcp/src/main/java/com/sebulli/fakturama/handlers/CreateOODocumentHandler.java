3c88d9f201b0320170a6c59cf428bc5fb2981c34
Ralf Heydenreich <ralf.heydenreich@fakturama.net>
2018-06-25 13:44:09 +0200

    added missing copyright infos

--- Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/handlers/CreateOODocumentHandler.java
+++ Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/handlers/CreateOODocumentHandler.java
@@ -1,14 +1,15 @@
-/*
- * Fakturama - Free Invoicing Software - http://fakturama.sebulli.com
+/* 
+ * Fakturama - Free Invoicing Software - http://www.fakturama.org
  * 
- * Copyright (C) 2012 Gerd Bartelt
+ * Copyright (C) 2014 www.fakturama.org
  * 
- * All rights reserved. This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License v1.0 which
- * accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: Gerd Bartelt - initial API and implementation
+ * Contributors:
+ *     The Fakturama Team - initial API and implementation
  */
 
 package com.sebulli.fakturama.handlers;
