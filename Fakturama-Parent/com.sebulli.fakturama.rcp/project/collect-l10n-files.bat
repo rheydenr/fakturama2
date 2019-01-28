901876ce0180fc1fef21a6b16ecb773a1599d7ad
Ralf Heydenreich <ralf.heydenreich@itemis.de>
2019-01-28 10:49:54 +0100

    added l10n collector file; Oomph setup updated

---
+++ Fakturama-Parent/com.sebulli.fakturama.rcp/project/collect-l10n-files.bat
@@ -0,0 +1,29 @@
+@REM Collects all the l10n files that are necessary for Fakturama
+@REM and puts them in a directory (separated by plugin project).
+@REM That directory can be zipped and given to translators,
+@REM along with a description from ../../org.fakturama.help/howto/translate/howto_translate.md
+@REM or ../../org.fakturama.help/howto/translate/howto_translate_DE.md.
+
+@ECHO OFF
+SET FAKTURAMA_INSTALLDIR=c:\Projekte\fakturama-2\Fakturama-Parent
+
+@REM create a folder with name as current date
+SET COLLECTIONS_FOLDER=%date:~-4,4%%date:~-7,2%%date:~-10,2%_Fakturama_l10nfiles
+mkdir %COLLECTIONS_FOLDER%
+cd %COLLECTIONS_FOLDER%
+
+mkdir rcp
+xcopy /f %FAKTURAMA_INSTALLDIR%\com.sebulli.fakturama.rcp\OSGI-INF\l10n\*.properties rcp
+move rcp\i18nedit.properties .
+
+mkdir import
+xcopy /f %FAKTURAMA_INSTALLDIR%\org.fakturama.import\OSGI-INF\l10n\*.properties import
+
+mkdir export
+xcopy /f %FAKTURAMA_INSTALLDIR%\org.fakturama.export\OSGI-INF\l10n\*.properties export
+
+mkdir zugferd
+xcopy /f %FAKTURAMA_INSTALLDIR%\org.fakturama.exporter.zugferd\OSGI-INF\l10n\*.properties zugferd
+
+cd ..
+
