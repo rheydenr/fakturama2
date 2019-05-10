2d880e3e7495fcd536badf6690c865a68c4f8481
Ralf Heydenreich <rheydenr@justmail.de>
2019-05-10 22:20:50 +0200

    756: Widget disposed / Absturz
    Task-Url: https://bugs.fakturama.info/view.php?id=756
    - checked date spinner if it is disposed

--- Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/parts/DocumentEditor.java
+++ Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/parts/DocumentEditor.java
@@ -1743,14 +1743,11 @@ public class DocumentEditor extends Editor<Document> {
         if (dtIssueDate != null && dtDate.getSelection() != null) {
             Calendar calendar = Calendar.getInstance();
             calendar.setTime(dtDate.getSelection());
-            //	    LocalDate localDate = LocalDate.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
-            //	    localDate.plusDays(spDueDays.getSelection());
-            //	    Date.from(localDate.???)
-            //		GregorianCalendar calendar = new GregorianCalendar(dtDate.getYear(), dtDate.getMonth(), dtDate.getDay());
-            
 			top.getParent().getDisplay().syncExec(() -> {
-				calendar.add(Calendar.DAY_OF_MONTH, spDueDays.getSelection());
-				dtIssueDate.setSelection(calendar.getTime());
+			    if(!spDueDays.isDisposed()) {
+    				calendar.add(Calendar.DAY_OF_MONTH, spDueDays.getSelection());
+    				dtIssueDate.setSelection(calendar.getTime());
+			    }
 			});
         }
     }
