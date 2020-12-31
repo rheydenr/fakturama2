4491897b057467b98decd69a7a4ad7f73245757a
Ralf Heydenreich <rheydenr@justmail.de>
2020-12-31 03:10:36 +0100

    855: Neuberechnung der Dokument-Summen implementieren
    Task-Url: https://bugs.fakturama.info/view.php?id=855
    - test setting fixed (is now JUnit 5)

--- Fakturama-Parent/com.sebulli.fakturama.rcp.test/src/test/java/com/sebulli/fakturama/office/PlaceholdersTest.java
+++ Fakturama-Parent/com.sebulli.fakturama.rcp.test/src/test/java/com/sebulli/fakturama/office/PlaceholdersTest.java
@@ -5,64 +5,63 @@ import org.junit.jupiter.api.Test;
 
 public class PlaceholdersTest {
 
-	@Test
-	public void testInterpretParameters_placeholderHasNoParams_Success() {
-		Placeholders p = new Placeholders();
-		Assertions.assertEquals("placeholder has no parameter!", "BOO", p.interpretParameters("DINGSDA", "BOO"));
-	}
-	
-	@Test
-	public void testInterpretParameters_placeholderHasPreParam_Success() {
-		Placeholders p = new Placeholders();
-		Assertions.assertEquals("placeholder was not correctly substituted with param!", "FooBar", p.interpretParameters("DINGSDA$PRE:Foo", "Bar"));
-	}
-	
-	@Test
-	public void testInterpretParameters_placeholderHasFirstParam_Success() {
-		Placeholders p = new Placeholders();
-		Assertions.assertEquals("1. placeholder was not correctly substituted with param!", "Foo", p.interpretParameters("DINGSDA$FIRST:3", "FooBar"));
-		Assertions.assertEquals("2. placeholder was not correctly substituted with param!", "FooBar", p.interpretParameters("DINGSDA$FIRST:8", "FooBar"));
-		Assertions.assertEquals("3. placeholder was not correctly substituted with param!", "", p.interpretParameters("DINGSDA$FIRST:0", "FooBar"));
-		Assertions.assertEquals("4. placeholder was not correctly substituted with param!", "FooBar", p.interpretParameters("DINGSDA$FIRST:nodeal", "FooBar"));
-	}
+    @Test
+    public void testInterpretParameters_placeholderHasNoParams_Success() {
+        Placeholders p = new Placeholders();
+        Assertions.assertEquals("BOO", p.interpretParameters("DINGSDA", "BOO"), "placeholder has no parameter!");
+    }
 
-	@Test
-	public void testInterpretParameters_placeholderHasLastParam_Success() {
-		Placeholders p = new Placeholders();
-		Assertions.assertEquals("1. placeholder was not correctly substituted with param!", "Bar", p.interpretParameters("DINGSDA$LAST:3", "FooBar"));
-		Assertions.assertEquals("2. placeholder was not correctly substituted with param!", "FooBar", p.interpretParameters("DINGSDA$LAST:8", "FooBar"));
-		Assertions.assertEquals("3. placeholder was not correctly substituted with param!", "", p.interpretParameters("DINGSDA$LAST:0", "FooBar"));
-		Assertions.assertEquals("4. placeholder was not correctly substituted with param!", "FooBar", p.interpretParameters("DINGSDA$LAST:nodeal", "FooBar"));
-	}
-	
-	@Test
-	public void testInterpretParameters_placeholderHasRangeParam_Success() {
-		Placeholders p = new Placeholders();
-		Assertions.assertEquals("1. placeholder was not correctly substituted with param!", "ooB", p.interpretParameters("DINGSDA$RANGE:2,4", "FooBar"));
-		Assertions.assertEquals("2. placeholder was not correctly substituted with param!", "ooBar", p.interpretParameters("DINGSDA$RANGE:2,8", "FooBar"));
-		Assertions.assertEquals("3. placeholder was not correctly substituted with param!", "", p.interpretParameters("DINGSDA$RANGE:2,0", "FooBar"));
-		Assertions.assertEquals("4. placeholder was not correctly substituted with param!", "ooBar", p.interpretParameters("DINGSDA$RANGE:2,nodeal", "FooBar"));
-	}
-	
-	@Test
-	public void testInterpretParameters_placeholderHasExRangeParam_Success() {
-		Placeholders p = new Placeholders();
-		Assertions.assertEquals("1. placeholder was not correctly substituted with param!", "Far", p.interpretParameters("DINGSDA$EXRANGE:2,4", "FooBar"));
-		Assertions.assertEquals("2. placeholder was not correctly substituted with param!", "F", p.interpretParameters("DINGSDA$EXRANGE:2,8", "FooBar"));
-		Assertions.assertEquals("3. placeholder was not correctly substituted with param!", "FoBar", p.interpretParameters("DINGSDA$EXRANGE:2,2", "FooBar"));
-		Assertions.assertEquals("4. placeholder was not correctly substituted with param!", "", p.interpretParameters("DINGSDA$EXRANGE:0,6", "FooBar"));
-		Assertions.assertEquals("5. placeholder was not correctly substituted with param!", "F", p.interpretParameters("DINGSDA$EXRANGE:2,nodeal", "FooBar"));
-	}
+    @Test
+    public void testInterpretParameters_placeholderHasPreParam_Success() {
+        Placeholders p = new Placeholders();
+        Assertions.assertEquals("FooBar", p.interpretParameters("DINGSDA$PRE:Foo", "Bar"), "placeholder was not correctly substituted with param!");
+    }
 
-	
-//	@Test
-//	public void testExtractParam() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testIsPlaceholder() {
-//		fail("Not yet implemented");
-//	}
+    @Test
+    public void testInterpretParameters_placeholderHasFirstParam_Success() {
+        Placeholders p = new Placeholders();
+        Assertions.assertEquals("Foo", p.interpretParameters("DINGSDA$FIRST:3", "FooBar"), "1. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("FooBar", p.interpretParameters("DINGSDA$FIRST:8", "FooBar"), "2. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("", p.interpretParameters("DINGSDA$FIRST:0", "FooBar"), "3. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("FooBar", p.interpretParameters("DINGSDA$FIRST:nodeal", "FooBar"), "4. placeholder was not correctly substituted with param!");
+    }
+
+    @Test
+    public void testInterpretParameters_placeholderHasLastParam_Success() {
+        Placeholders p = new Placeholders();
+        Assertions.assertEquals("Bar", p.interpretParameters("DINGSDA$LAST:3", "FooBar"), "1. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("FooBar", p.interpretParameters("DINGSDA$LAST:8", "FooBar"), "2. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("", p.interpretParameters("DINGSDA$LAST:0", "FooBar"), "3. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("FooBar", p.interpretParameters("DINGSDA$LAST:nodeal", "FooBar"), "4. placeholder was not correctly substituted with param!");
+    }
+
+    @Test
+    public void testInterpretParameters_placeholderHasRangeParam_Success() {
+        Placeholders p = new Placeholders();
+        Assertions.assertEquals("ooB", p.interpretParameters("DINGSDA$RANGE:2,4", "FooBar"), "1. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("ooBar", p.interpretParameters("DINGSDA$RANGE:2,8", "FooBar"), "2. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("", p.interpretParameters("DINGSDA$RANGE:2,0", "FooBar"), "3. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("ooBar", p.interpretParameters("DINGSDA$RANGE:2,nodeal", "FooBar"), "4. placeholder was not correctly substituted with param!");
+    }
+
+    @Test
+    public void testInterpretParameters_placeholderHasExRangeParam_Success() {
+        Placeholders p = new Placeholders();
+        Assertions.assertEquals("Far", p.interpretParameters("DINGSDA$EXRANGE:2,4", "FooBar"), "1. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("F", p.interpretParameters("DINGSDA$EXRANGE:2,8", "FooBar"), "2. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("FoBar", p.interpretParameters("DINGSDA$EXRANGE:2,2", "FooBar"), "3. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("", p.interpretParameters("DINGSDA$EXRANGE:0,6", "FooBar"), "4. placeholder was not correctly substituted with param!");
+        Assertions.assertEquals("F", p.interpretParameters("DINGSDA$EXRANGE:2,nodeal", "FooBar"), "5. placeholder was not correctly substituted with param!");
+    }
+
+    //	@Test
+    //	public void testExtractParam() {
+    //		fail("Not yet implemented");
+    //	}
+    //
+    //	@Test
+    //	public void testIsPlaceholder() {
+    //		fail("Not yet implemented");
+    //	}
 
 }
