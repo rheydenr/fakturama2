b24afe626fe526d8315811f6b3a0d7f0a064261e
Ralf Heydenreich <rheydenr@justmail.de>
2021-01-25 21:33:52 +0100

    855: Neuberechnung der Dokument-Summen implementieren
    Task-Url: https://bugs.fakturama.info/view.php?id=855
    - Added PriceBuilder

--- Fakturama-Parent/com.sebulli.fakturama.rcp.test/src/test/java/com/sebulli/fakturama/calculate/DocumentSummaryCalcTest.java
+++ Fakturama-Parent/com.sebulli.fakturama.rcp.test/src/test/java/com/sebulli/fakturama/calculate/DocumentSummaryCalcTest.java
@@ -1,5 +1,8 @@
 package com.sebulli.fakturama.calculate;
 
+import java.util.spi.ResourceBundleProvider;
+
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.services.nls.IMessageFactoryService;
@@ -12,6 +15,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import com.sebulli.fakturama.dto.Price;
+import com.sebulli.fakturama.dto.PriceBuilder;
 import com.sebulli.fakturama.i18n.Messages;
 
 public class DocumentSummaryCalcTest {
@@ -20,20 +24,28 @@ public class DocumentSummaryCalcTest {
 
     @Mock
     private IMessageFactoryService factoryService;
+    
+    @Mock
+    private ResourceBundleProvider provider;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         ctx = EclipseContextFactory.create("test-context");
         Mockito.when(factoryService.getMessageInstance(Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(new Messages());
+        
         ctx.set(IMessageFactoryService.class, factoryService);
+        ctx.set(ResourceBundleProvider.class, provider);
+        ContextInjectionFactory.setDefault(ctx);
     }
 
     @Test
     public void testCalculateSummary() {
-        
-        Price p = new Price(Money.of(MoneyUtils.getBigDecimal(25), "EUR"));
-        System.out.println(p);
+        Price testPrice = new PriceBuilder()
+                .withAmount(Money.of(MoneyUtils.getBigDecimal(25), "EUR"))
+                .withVatPercent(0.19).build();
+//        Price p = new Price(Money.of(MoneyUtils.getBigDecimal(25), "EUR"));
+        System.out.println(testPrice);
 
     }
 
