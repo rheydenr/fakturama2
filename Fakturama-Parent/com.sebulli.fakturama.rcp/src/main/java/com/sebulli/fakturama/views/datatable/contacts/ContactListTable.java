dc5d256f60c4d0b833a444743032c5cc37789d68
Ralf Heydenreich <rheydenr@justmail.de>
2021-04-15 21:34:21 +0200

    FAK-924: Suchfunktion bei Kontakten funktioniert nicht korrekt
    - filter fixed

--- Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/views/datatable/contacts/ContactListTable.java
+++ Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/views/datatable/contacts/ContactListTable.java
@@ -318,9 +318,10 @@ public abstract class ContactListTable<T extends Contact> extends AbstractViewDa
         final MatcherEditor<T> textMatcherEditor = createTextWidgetMatcherEditor();
         
         // Filtered list for Search text field filter
+        final FilterList<T> textFilteredIssues = new FilterList<T>(contactListData, textMatcherEditor);
         // build the list for the tree-filtered values (i.e., the value list which is affected by
         // tree selection)
-        treeFilteredIssues = new FilterList<T>(contactListData, textMatcherEditor);
+        treeFilteredIssues = new FilterList<T>(textFilteredIssues);
         
         //build the grid layer
         setGridLayer(new EntityGridListLayer<T>(treeFilteredIssues, propertyNames, derivedColumnPropertyAccessor, configRegistry));
