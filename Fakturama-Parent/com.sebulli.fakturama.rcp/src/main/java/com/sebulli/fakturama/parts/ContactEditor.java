5ddbd0a589c5951986a560e3b73c6ebd02ec0dba
Ralf Heydenreich <rheydenr@justmail.de>
2021-05-24 01:43:07 +0200

    FAK-953: Scrolleiste in der Länder-Liste
    - in der Dropdownbox kann man jetzt durch Eingabe der Buchstaben zum
    entsprechenden Eintrag springen. Die Übernahme des Eintrages erfolgt per
    Mausklick oder Enter.

--- Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/parts/ContactEditor.java
+++ Fakturama-Parent/com.sebulli.fakturama.rcp/src/main/java/com/sebulli/fakturama/parts/ContactEditor.java
@@ -59,7 +59,6 @@ import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
